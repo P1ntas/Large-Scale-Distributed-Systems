6289990a942d72e29b5ca5660d16f1f6004180bb
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-11-22 11:31:59 +0000

    added vector clocks

--- crdt/src/main/java/Product.java
+++ crdt/src/main/java/Product.java
@@ -4,17 +4,20 @@ public class Product {
     UUID id;
     String name;
     int quantity;
+    VectorClock vectorClock;
 
     public Product(String name) {
         this.id = UUID.randomUUID();
         this.name = name;
         this.quantity = 1;
+        this.vectorClock = new VectorClock(id, System.currentTimeMillis());
     }
 
-    public Product(String name, int quantity) {
+    public Product(String name, int quantity, VectorClock vectorClock) {
         this.id = UUID.randomUUID();
         this.name = name;
         this.quantity = quantity;
+        this.vectorClock = vectorClock;
     }
 
     public UUID getId() {
@@ -29,6 +32,10 @@ public class Product {
         return this.quantity;
     }
 
+    public VectorClock getVectorClock() {
+        return this.vectorClock;
+    }
+
     //setters
     public void setName(String name) {
         this.name = name;
@@ -41,6 +48,10 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public void setVectorClock(VectorClock vectorClock) {
+        this.vectorClock = vectorClock;
+    }
+
     // Methods
     public void incrementQuantity() {
         this.quantity++;
@@ -51,5 +62,28 @@ public class Product {
             throw new IllegalArgumentException("Quantity cannot be negative");
         }
         this.quantity--;
+    }
+
+    public void mergeProduct(Product other) {
+        // Concatenating the names
+        if (!this.name.equals(other.name)) {
+            if (other.name.contains(this.name)) {
+                this.name = other.name;
+            }
+            else if (!this.name.contains(other.name)) {
+                this.name = this.name + other.name;
+            }
+        }
+
+        // Determining which product has the larger vector clock
+        boolean thisClockIsGreater = this.vectorClock.isGreaterThan(other.vectorClock);
+
+        if (!thisClockIsGreater) {
+            // If the other product has the larger clock, adopt its quantity
+            this.quantity = other.quantity;
+        }
+
+        // Merge the vector clocks
+        this.vectorClock.merge(other.vectorClock);
     }
 }
