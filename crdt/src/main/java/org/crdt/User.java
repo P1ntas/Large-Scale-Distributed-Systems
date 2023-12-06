5ea75c8a8f91e5a9b472cbb26cb3b6e46a2ae751
dfneves19 <41472620+dfneves19@users.noreply.github.com>
2023-12-06 02:19:07 +0000

    Changed pncounter to products

--- crdt/src/main/java/org/crdt/User.java
+++ crdt/src/main/java/org/crdt/User.java
@@ -6,26 +6,25 @@ public class User {
     String username;
     UUID id;
     HashMap<UUID, ShoppingList> shoppingLists;
-    PNCounter counter;
+
 
     public User(String username) {
         this.username = username;
         this.id = UUID.randomUUID();
-        this.counter = new PNCounter();
+
         this.shoppingLists = new HashMap<>();
     }
 
     public User(String username, UUID id, HashMap<UUID, ShoppingList> shoppingLists) {
         this.username = username;
         this.id = UUID.randomUUID();
-        this.counter = new PNCounter();
+
         this.shoppingLists = shoppingLists == null ? new HashMap<>() : shoppingLists;
     }
 
     public User(String username, UUID id) {
         this.username = username;
         this.id = id;
-        this.counter = new PNCounter();
         this.shoppingLists = new HashMap<>();
     }
 
@@ -37,9 +36,6 @@ public class User {
         return this.id;
     }
 
-    public PNCounter getCounter() {
-        return this.counter;
-    }
 
     public HashMap<UUID, ShoppingList> getShoppingLists() {
         return this.shoppingLists;
@@ -50,7 +46,4 @@ public class User {
         this.username = username;
     }
 
-    public void setPNCounter(PNCounter counter) {
-        this.counter = counter;
-    }
 }
